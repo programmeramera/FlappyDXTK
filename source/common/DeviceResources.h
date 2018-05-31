30f97d31673cb56dcf63824141aa0b9ec576591f
Johan Lindfors <johan.lindfors@coderox.se>
2018-05-31 08:53:44 +0200

    Initial commit

---
+++ source/common/DeviceResources.h
@@ -0,0 +1,103 @@
+﻿#pragma once
+#include "pch.h"
+
+namespace DX
+{
+	// Provides an interface for an application that owns DeviceResources to be notified of the device being lost or created.
+	interface IDeviceNotify
+	{
+		virtual void OnDeviceLost() = 0;
+		virtual void OnDeviceRestored() = 0;
+	};
+
+	// Controls all the DirectX device resources.
+	class DeviceResources
+	{
+	public:
+		DeviceResources();
+		void SetWindow(winrt::Windows::UI::Core::CoreWindow const& window);
+		void SetLogicalSize(winrt::Windows::Foundation::Size const& logicalSize);
+		void SetCurrentOrientation(winrt::Windows::Graphics::Display::DisplayOrientations const& currentOrientation);
+		void SetDpi(float dpi);
+		void ValidateDevice();
+		void HandleDeviceLost();
+		void RegisterDeviceNotify(IDeviceNotify* deviceNotify);
+		void Trim();
+		void Present();
+
+		// The size of the render target, in pixels.
+		winrt::Windows::Foundation::Size	GetOutputSize() const { return m_outputSize; }
+
+		// The size of the render target, in dips.
+		winrt::Windows::Foundation::Size	GetLogicalSize() const { return m_logicalSize; }
+		float						GetDpi() const { return m_effectiveDpi; }
+
+		// D3D Accessors.
+		ID3D11Device3*				GetD3DDevice() const { return winrt::get_abi(m_d3dDevice); }
+		ID3D11DeviceContext3*		GetD3DDeviceContext() const { return winrt::get_abi(m_d3dContext); }
+		IDXGISwapChain3*			GetSwapChain() const { return winrt::get_abi(m_swapChain); }
+		D3D_FEATURE_LEVEL			GetDeviceFeatureLevel() const { return m_d3dFeatureLevel; }
+		ID3D11RenderTargetView1*	GetBackBufferRenderTargetView() const { return winrt::get_abi(m_d3dRenderTargetView); }
+		ID3D11DepthStencilView*		GetDepthStencilView() const { return winrt::get_abi(m_d3dDepthStencilView); }
+		D3D11_VIEWPORT				GetScreenViewport() const { return m_screenViewport; }
+		DirectX::XMFLOAT4X4			GetOrientationTransform3D() const { return m_orientationTransform3D; }
+
+		// D2D Accessors.
+		ID2D1Factory3*				GetD2DFactory() const { return winrt::get_abi(m_d2dFactory); }
+		ID2D1Device2*				GetD2DDevice() const { return winrt::get_abi(m_d2dDevice); }
+		ID2D1DeviceContext2*		GetD2DDeviceContext() const { return winrt::get_abi(m_d2dContext); }
+		ID2D1Bitmap1*				GetD2DTargetBitmap() const { return winrt::get_abi(m_d2dTargetBitmap); }
+		IDWriteFactory3*			GetDWriteFactory() const { return winrt::get_abi(m_dwriteFactory); }
+		IWICImagingFactory2*		GetWicImagingFactory() const { return winrt::get_abi(m_wicFactory); }
+		D2D1::Matrix3x2F			GetOrientationTransform2D() const { return m_orientationTransform2D; }
+
+	private:
+		void CreateDeviceIndependentResources();
+		void CreateDeviceResources();
+		void CreateWindowSizeDependentResources();
+		void UpdateRenderTargetSize();
+		DXGI_MODE_ROTATION ComputeDisplayRotation();
+
+		// Direct3D objects.
+		winrt::com_ptr<ID3D11Device3>			m_d3dDevice;
+		winrt::com_ptr<ID3D11DeviceContext3>	m_d3dContext;
+		winrt::com_ptr<IDXGISwapChain3>			m_swapChain;
+
+		// Direct3D rendering objects. Required for 3D.
+		winrt::com_ptr<ID3D11RenderTargetView1>	m_d3dRenderTargetView;
+		winrt::com_ptr<ID3D11DepthStencilView>	m_d3dDepthStencilView;
+		D3D11_VIEWPORT							m_screenViewport;
+
+		// Direct2D drawing components.
+		winrt::com_ptr<ID2D1Factory3>		m_d2dFactory;
+		winrt::com_ptr<ID2D1Device2>		m_d2dDevice;
+		winrt::com_ptr<ID2D1DeviceContext2>	m_d2dContext;
+		winrt::com_ptr<ID2D1Bitmap1>		m_d2dTargetBitmap;
+
+		// DirectWrite drawing components.
+		winrt::com_ptr<IDWriteFactory3>		m_dwriteFactory;
+		winrt::com_ptr<IWICImagingFactory2>	m_wicFactory;
+
+		// Cached reference to the Window.
+		winrt::agile_ref<winrt::Windows::UI::Core::CoreWindow> m_window;
+
+		// Cached device properties.
+		D3D_FEATURE_LEVEL										m_d3dFeatureLevel;
+		winrt::Windows::Foundation::Size						m_d3dRenderTargetSize;
+		winrt::Windows::Foundation::Size						m_outputSize;
+		winrt::Windows::Foundation::Size						m_logicalSize;
+		winrt::Windows::Graphics::Display::DisplayOrientations	m_nativeOrientation;
+		winrt::Windows::Graphics::Display::DisplayOrientations	m_currentOrientation;
+		float													m_dpi;
+
+		// This is the DPI that will be reported back to the app. It takes into account whether the app supports high resolution screens or not.
+		float m_effectiveDpi;
+
+		// Transforms used for display orientation.
+		D2D1::Matrix3x2F	m_orientationTransform2D;
+		DirectX::XMFLOAT4X4	m_orientationTransform3D;
+
+		// The IDeviceNotify can be held directly as it owns the DeviceResources.
+		IDeviceNotify* m_deviceNotify;
+	};
+}
---
+++ source/common/DirectXHelper.h
@@ -0,0 +1,71 @@
+﻿#pragma once
+
+#include <ppltasks.h>	// For create_task
+#include <future>
+#include "pch.h"
+
+using namespace winrt::Windows::Foundation;
+using namespace winrt::Windows::Storage;
+using namespace winrt::Windows::Storage::Streams;
+
+namespace DX
+{
+	template <typename From, typename To>
+	void As(From const & from, winrt::com_ptr<To> & to)
+	{
+		to = from.as<To>();
+	}
+
+	inline void ThrowIfFailed(HRESULT hr)
+	{
+		if (FAILED(hr))
+		{
+			// Set a breakpoint on this line to catch Win32 API errors.
+			throw winrt::hresult_error(hr);
+		}
+	}
+
+	inline std::future<std::vector<byte>> ReadDataAsync(const std::wstring& filename)
+	{
+		try {
+			auto folder = winrt::Windows::ApplicationModel::Package::Current().InstalledLocation();
+			auto file = co_await folder.GetFileAsync(winrt::hstring(filename.c_str()));
+			IBuffer fileBuffer = co_await FileIO::ReadBufferAsync(file);
+			std::vector<byte> returnBuffer;
+			returnBuffer.resize(fileBuffer.Length());
+			DataReader::FromBuffer(fileBuffer).ReadBytes(winrt::array_view<byte>(returnBuffer));
+			return returnBuffer;
+		}
+		catch (...) {
+
+		}
+	}
+
+	// Converts a length in device-independent pixels (DIPs) to a length in physical pixels.
+	inline float ConvertDipsToPixels(float dips, float dpi)
+	{
+		static const float dipsPerInch = 96.0f;
+		return floorf(dips * dpi / dipsPerInch + 0.5f); // Round to nearest integer.
+	}
+
+#if defined(_DEBUG)
+	// Check for SDK Layer support.
+	inline bool SdkLayersAvailable()
+	{
+		HRESULT hr = D3D11CreateDevice(
+			nullptr,
+			D3D_DRIVER_TYPE_NULL,       // There is no need to create a real hardware device.
+			0,
+			D3D11_CREATE_DEVICE_DEBUG,  // Check for the SDK layers.
+			nullptr,                    // Any feature level will do.
+			0,
+			D3D11_SDK_VERSION,          // Always set this to D3D11_SDK_VERSION for Windows Store apps.
+			nullptr,                    // No need to keep the D3D device reference.
+			nullptr,                    // No need to know the feature level.
+			nullptr                     // No need to keep the D3D device context reference.
+		);
+
+		return SUCCEEDED(hr);
+	}
+#endif
+}
